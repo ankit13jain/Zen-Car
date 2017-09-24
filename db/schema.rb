a3b2d6e28bd61b9f06de56c00fec78f17a8108d2
Dax Amin <dkamin@ncsu.edu>
2017-09-24 16:15:01 -0400

    New branch

--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170922230129) do
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.string "author_type"
-    t.integer "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
   create_table "cars", force: :cascade do |t|
     t.string "license_no"
     t.string "manufacturer"
