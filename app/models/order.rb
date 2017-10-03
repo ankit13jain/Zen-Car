54adbdac2afa932b7fd6ed2c38a78cb134e7a6b5
Dax Amin <dkamin@ncsu.edu>
2017-10-02 21:47:25 -0400

    Order history | Back navigations | Cancel order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :car
   belongs_to :customer
 
-  STATUS = ["Initiated", "In Progress", "Completed"]
+  STATUS = ["Initiated", "In Progress", "Completed", "Canceled"]
   def self.search(params)
     where(['cast(customer_id as text) LIKE ?', "%#{params[:cust_id]}%"])
   end
