78ad41eb69727d6747435b0438e770597f13ba1f
Nirav Jain <najain@ncsu.edu>
2017-10-03 23:59:43 -0400

    Fixed merge problems

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,35 +6,12 @@ class Order < ApplicationRecord
   def self.search(params)
 
     # If params contains car id, then consider car id also while displaying results
+    p params
     if params.has_key? :car_id
       where(:car_id => params[:car_id])
     else
       where(:customer_id => params[:cust_id])
     end
-  end
-
-  def cancel_unchecked_car
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    p "+++++++++++++++++++++++"
-    Order.find(:id => 4).update(:status => "CANCELED")
   end
 
 end
